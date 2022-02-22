18c347b8de6f8ab121e641c7217380ddfcb94ced
Will <will.rains24@gmail.com>
2022-02-22 21:59:23 +0100

    printing the names to the container from the api data adding some javascript

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ fetch("https://www.balldontlie.io/api/v1/players", requestOptions)
     let container = document.querySelector('#container');
     container.innerHTML = ``;
 
-    console.log(result);
+    console.log(result,'result log');
+    console.log(result.data,'result.data log');
+    result.data.forEach(player => {
+
+      let resp = document.createElement('section');
+      resp.innerHTML = `<section>${player.first_name} ${player.last_name}</section>`;
+      container.appendChild(resp);
+      // const players = [];
+      // players.map(player);
+    });
+
     let resp = document.createElement('section');
     resp.innerHTML = `<section>${result}</section>`;
-    document.querySelector('#container').appendChild(resp);
+    container.appendChild(resp);
+
+
 
 
 })
