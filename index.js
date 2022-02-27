67d9b6f53268293b90cbc3b2f7f4293847e39853
Will <will.rains24@gmail.com>
2022-02-27 12:12:34 +0100

    spacing is correct and the first API connection is working as intended

--- index.js
+++ index.js
@@ -6,23 +6,20 @@ const requestOptions = {
 fetch("https://www.balldontlie.io/api/v1/players", requestOptions)
   .then(response => response.json())
   .then(result => {
-    let container = document.querySelector('#container');
-    container.innerHTML = ``;
+    document.querySelector("footer").style.visibility="hidden";
+    let container = document.querySelector('main');
+    container.style.height = "";
 
     console.log(result,'result log');
     console.log(result.data,'result.data log');
     result.data.forEach(player => {
 
       let resp = document.createElement('section');
-      resp.innerHTML = `<section>${player.first_name} ${player.last_name}</section>`;
+      resp.innerHTML = `<section class= "player-divs">${player.first_name} ${player.last_name}</section>`;
       container.appendChild(resp);
       // const players = [];
       // players.map(player);
     });
-
-    let resp = document.createElement('section');
-    resp.innerHTML = `<section>${result}</section>`;
-    container.appendChild(resp);
 
 
 
