adf68082845949da17c6d45c85ac9f9051fa7951
Will <will.rains24@gmail.com>
2022-03-03 23:47:21 +0100

    getting closer to having tim dunanc show to start the app

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+//read in the value inputted in the search
+let searchValue=document.getElementById('search-input').value;
+
+//GET request to get Tim Duncan - NEW START OF FILE
+fetch(`https://www.balldontlie.io/api/v1/players?search=Tim Duncan`, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+
+    //hide footer
+    document.querySelector("footer").style.visibility="hidden";
+
+    //query select container
+    let container = document.querySelector('main');
+
+    console.log(result,'result log');
+    console.log(result.data,'result.data log');
+      //create section for each player
+      let resp = document.createElement('section');
+      resp.classList.add("player-div")
+
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      const timD = result.data;
+      console.log(timD[0],'timD log');
+      let timIdArray = [];
+      console.log(timIdArray,'timIdArray1 log');
+      timIdArray.push(timD[0].id);
+      console.log(timIdArray,'timIdArray2 log');
+
+      fetch(`https://www.balldontlie.io/api/v1/stats?player_ids=[${timD[0].id}]`, requestOptions)
+        .then(response => response.json())
+        .then(resultInner => {
+
+          resp.innerHTML =
+          `
+          <h2>🏀 ${result.first_name} ${result.last_name} </h2>
+          <p>  </p>
+          `;
+          container.appendChild(resp);
+
+          })
+        .catch(error => console.log('error', error))
+      })
+  .catch(error => console.log('error', error));
+
+
 
 //declare searchbar element using DOM
 let searchBar=document.querySelector("#search-bar");
@@ -11,9 +63,11 @@ searchBar.addEventListener("submit",function (e) {
       method: 'GET',
       redirect: 'follow'
     };
+    //read in the value inputted in the search
+    let searchValue=document.getElementById('search-input').value;
 
     //GET request to get the players data
-    fetch("https://www.balldontlie.io/api/v1/players", requestOptions)
+    fetch(`https://www.balldontlie.io/api/v1/players?search=${searchValue}`, requestOptions)
       .then(response => response.json())
       .then(result => {
 
